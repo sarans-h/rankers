6d3f11608a04a3c024c973693109d5ad8e4982d4
Saransh Gupta <113707195+sarans-h@users.noreply.github.com>
2024-07-20 08:03:56 +0530

    Delete service directory

--- service/auth.js
+++
@@ -1,8 +0,0 @@
-const sessionIdToUserMap=new Map();
-function setUser(id,user) {
-    sessionIdToUserMap.set(id,user)
-}
-function getUser(id) {
-    return sessionIdToUserMap.get(id);
-}
-module.exports={setUser,getUser}
